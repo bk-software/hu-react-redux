7c0c55481b25950a5739947c45a48fdf105709d9
bk-software <barak.software.solutions@gmail.com>
2022-12-14 21:02:08 +0200

    Add designed error page

--- react-apps/src/applications/real-app/503_App_add_page_header/pages/ErrorPage.jsx
+++ react-apps/src/applications/real-app/503_App_add_page_header/pages/ErrorPage.jsx
@@ -1,8 +1,26 @@
 import Layout from '../layout/Layout';
+import PageHeader from '../components/PageHeader';
+import { Typography, Grid } from '@mui/material';
+
 function ErrorPage() {
   return (
     <Layout>
-      <div>Error Stub</div>
+      <PageHeader title="Error 404" subtitle="Page not found" />
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Typography variant="h5">
+            Oops... The requested URL was not found on this server
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <img
+            width="100%"
+            src="/assets/images/broken-robot.png"
+            alt="broken robot"
+          />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
