ccc39aad2308a07357275fbba33da93c2ca874e4
bk-software <barak.software.solutions@gmail.com>
2022-12-25 20:40:17 +0200

    use the custom hook in post view

--- react-apps/src/applications/homework/posts-comments/ver1/PostsView.jsx
+++ react-apps/src/applications/homework/posts-comments/ver1/PostsView.jsx
@@ -1,22 +1,11 @@
 import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
 import { Posts } from './Posts';
+import { useGetDataFromUrl } from './useGetDataFromUrl';
 
 //Wrapper Component for the posts
 export function PostsView() {
-  const [posts, setPosts] = useState();
-
-  useEffect(() => {
-    async function fetchPosts() {
-      const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
-      const response = await axios.get(postsUrl);
-      console.log({ response });
-      setPosts(response.data);
-    }
-
-    fetchPosts();
-  }, []);
+  const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+  const posts = useGetDataFromUrl(postsUrl);
 
   if (!posts) {
     return <div>Loading posts...</div>;
