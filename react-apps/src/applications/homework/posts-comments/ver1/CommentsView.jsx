3d6643583969debc0a31f31b7e83d0e8c5adbdd8
bk-software <barak.software.solutions@gmail.com>
2022-12-25 20:28:14 +0200

    add custom comments view

--- react-apps/src/applications/homework/posts-comments/ver1/CommentsView.jsx
+++ react-apps/src/applications/homework/posts-comments/ver1/CommentsView.jsx
@@ -2,19 +2,25 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Comments } from './Comments';
 
-export function CommentsView({ postId }) {
+function useGetDataFromUrl(commentsUrl) {
   const [comments, setComments] = useState();
 
   useEffect(() => {
     async function fetchComments() {
-      const commentsUrl = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
       const response = await axios.get(commentsUrl);
       console.log({ response });
       setComments(response.data);
     }
 
     fetchComments();
-  }, [postId]);
+  }, [commentsUrl]);
+
+  return comments;
+}
+
+export function CommentsView({ postId }) {
+  const commentsUrl = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+  const comments = useGetDataFromUrl(commentsUrl);
 
   if (!comments) {
     return <div>Loading comments ...</div>;
