05191ef12c3f757bd4d8b783ac16312ec2bac9bb
bk-software <barak.software.solutions@gmail.com>
2022-12-25 18:48:17 +0200

    toggle comments component

--- react-apps/src/applications/homework/posts-comments/ver1/Post.jsx
+++ react-apps/src/applications/homework/posts-comments/ver1/Post.jsx
@@ -1,8 +1,28 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+function Comments() {
+  return <div>My comments</div>;
+}
 export function Post({ post }) {
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleComments() {
+    setShowComments(!showComments);
+  }
+
   return (
-    <div style={{ border: '1px solid gray', paddingLeft: '10px' }}>
+    <div
+      onClick={toggleComments}
+      style={{ border: '1px solid gray', paddingLeft: '10px' }}
+    >
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      {showComments && <Comments />}
     </div>
   );
 }
+
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+};
